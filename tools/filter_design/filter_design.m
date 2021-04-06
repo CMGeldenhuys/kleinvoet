5315ad67db37e4558c3c8e8be9c445692e2ab198
CM Geldenhuys <20198329@sun.ac.za>
2021-04-06 19:20:11 +0200

    Better document filter design

--- tools/filter_design/filter_design.m
+++ tools/filter_design/filter_design.m
@@ -14,37 +14,48 @@ opts.PhaseVisible = 'off';
 
 % Create filter with ZPK with bessel polynomials
 [z, p, k] = besself(order, fo*2*pi);
+% -k due to MFB topology
 sys = zpk(z, p, -k);
-ax = axes();
-bodeplot(ax,sys, opts)
+ax1 = axes();
+bodeplot(ax1,sys, opts)
 
 hold on
 sys_gain = zpk(z, p, -k*gain)
-bodeplot(ax, sys_gain, opts)
-legend(ax, 'AA Filter', 'AA Filter (with gain)')
-grid(ax)
+bodeplot(ax1, sys_gain, opts)
+xline(ax1, fo*2*pi, '-.','$f_o$')
+legend(ax1, 'AA Filter', 'AA Filter (with gain)')
+set(ax1, 'FontSize', 10)
+xlabel(ax1, 'Frequency (rad/s)', 'FontSize', 10)
+ylabel(ax1, 'Magnitude (dB)', 'FontSize', 10)
+title(ax1, '')
+grid(ax1)
 hold off
 
 % Get num & den from zpk
 [b,a] = zp2tf(z, p, k*gain);
-% Analyse frequency response
-[h,w] = freqs(b,a,1000);
+% Calc. over same freq range as above
+w = log10(xlim);
+w = logspace(w(1), w(2), 1000);
+% Compute frequency response
+h = freqs(b, a, w);
 % Calculate groupdelay
 grpdel = -diff(unwrap(angle(h)))./diff(w);
 
 figure
-ax = axes();
-semilogx(ax, w(2:end),grpdel)
-xlabel(ax, 'Frequency (rad/s)')
-ylabel(ax, 'Group delay (s)')
-grid(ax)
+ax2 = axes;
+semilogx(ax2, w(2:end),grpdel * 1e6)
+xline(ax2, fo*2*pi, '-.','$f_o$')
+xlabel(ax2, 'Frequency (rad/s)')
+ylabel(ax2, 'Group delay (us)')
+grid(ax2)
 
-
+% Ensure x axis align
+linkaxes([ax1 ax2], 'x')
 %% Stability Margins
 
-figure
 % Plot stability margins
-margin(sys_gain)
+% figure
+% margin(sys_gain)
 % Get stability margins
 [Gm Pm Wcg Wcp] = margin(sys_gain)
 
